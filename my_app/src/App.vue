53663b8729a06d1de1218b95affce30c127078a1
margtr <1911678141@qq.com>
2019-12-17 19:07:28 +0800

    2

--- my_app/src/App.vue
+++ my_app/src/App.vue
@@ -1,32 +1,87 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  
     <router-view/>
+  <nav class="tabbar">
+        <div class="tabbar-item active">
+            <a href="" class="navigation">
+               <van-icon name="wap-home-o" style="font-size:24px" />
+                <p>首页</p>
+            </a>
+        </div>
+        <div class="tabbar-item">
+            <a href="" class="navigation">
+                <van-icon name="coupon-o" style="font-size:24px" />
+                <p>分类</p>
+            </a>
+        </div>
+        <div class="tabbar-item" data>
+            <a href="" class="navigation">
+                <van-icon name="cart-circle-o" style="font-size:24px"/>
+                <p>购物车</p>
+            </a>
+        </div>
+        <div class="tabbar-item">
+            <a href="" class="navigation">
+               <van-icon name="user-circle-o"  style="font-size:24px"/>
+                <p>我的</p>
+            </a>
+        </div>
+    </nav>
   </div>
 </template>
+<script>
+import Vue from 'vue';
+import { Icon } from 'vant';
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+Vue.use(Icon);
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'home',
+  components: {
+  
   }
 }
+</script>
+<style lang="less">
+#app {
+width: 100%;
+height: 100%;
+}
+.tabbar {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 9;
+    display: flex;
+    -webkit-box-align: center;
+        align-items: center;
+            height: 3.5rem;
+    overflow: hidden;
+    background: #F7F9FA;
+    border: 0;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "iconfont" !important;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+  -webkit-tap-highlight-color: transparent;
+  outline: none;
+  -webkit-focus-ring-color: transparent;
+}
+.tabbar-item.active {
+    color: #FF734C;
+}
+.tabbar-item{
+  flex: 1;
+  text-align: center;
+  -webkit-box-flex: 1;
+
+}
+
+
 </style>
