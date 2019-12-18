c523c5a60f23026ea4696e3b463a5bbb06f36504
margtr <1911678141@qq.com>
2019-12-18 09:14:55 +0800

    3

--- my_app/src/App.vue
+++ my_app/src/App.vue
@@ -1,33 +1,8 @@
 <template>
   <div id="app">
+  <router-view></router-view>
+    <tabBar></tabBar>
   
-    <router-view/>
-  <nav class="tabbar">
-        <div class="tabbar-item active">
-            <a href="" class="navigation">
-               <van-icon name="wap-home-o" style="font-size:24px" />
-                <p>首页</p>
-            </a>
-        </div>
-        <div class="tabbar-item">
-            <a href="" class="navigation">
-                <van-icon name="coupon-o" style="font-size:24px" />
-                <p>分类</p>
-            </a>
-        </div>
-        <div class="tabbar-item" data>
-            <a href="" class="navigation">
-                <van-icon name="cart-circle-o" style="font-size:24px"/>
-                <p>购物车</p>
-            </a>
-        </div>
-        <div class="tabbar-item">
-            <a href="" class="navigation">
-               <van-icon name="user-circle-o"  style="font-size:24px"/>
-                <p>我的</p>
-            </a>
-        </div>
-    </nav>
   </div>
 </template>
 <script>
@@ -35,11 +10,11 @@ import Vue from 'vue';
 import { Icon } from 'vant';
 
 Vue.use(Icon);
-
+import tabBar from "./components/tabBar.vue"
 export default {
   name: 'home',
   components: {
-  
+  tabBar
   }
 }
 </script>
@@ -55,45 +30,4 @@ body {
     font-size: 14px;
     box-sizing: border-box;
 }
-.tabbar {
-    position: fixed;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 9;
-    display: flex;
-    -webkit-box-align: center;
-        align-items: center;
-            height: 3.5rem;
-    overflow: hidden;
-    background: #F7F9FA;
-    border: 0;
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    font-family: "iconfont" !important;
-}
-a {
-  text-decoration: none;
-  color: inherit;
-  -webkit-tap-highlight-color: transparent;
-  outline: none;
-  -webkit-focus-ring-color: transparent;
-}
-.tabbar-item.active {
-    color: #FF734C;
-}
-.tabbar-item{
-  flex: 1;
-  text-align: center;
-  -webkit-box-flex: 1;
-
-}
-.tabbar-item > .navigation {
-  height: 3.5rem;
-  padding-top: 0.28571429rem;
-  box-sizing: border-box;
-}
-
-
 </style>
